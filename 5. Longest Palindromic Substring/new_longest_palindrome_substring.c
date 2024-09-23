acec8d0bfa1f7b02f32960f1afeb9636237f70c0
Holm Cramer <holmcramer@gmail.com>
2024-09-23 16:26:38 +0200

    only a few bugs left

--- 5. Longest Palindromic Substring/new_longest_palindrome_substring.c
+++ 5. Longest Palindromic Substring/new_longest_palindrome_substring.c
@@ -2,15 +2,18 @@
 
 #include <string.h>
 #include <stdio.h>
+#include <malloc.h>
 
-
-int test(char* s, char* resultStr)
+// aka, akska funzt aber aa oder a nicht
+char* longestPalindrome(char* s)
 {
+    char resultStr[strlen(s)+1];
     char* left_ptr = &s[0];
     char* left_inner_ptr;
     char* right_ptr = &s[1];
     char* right_inner_ptr;
-    char substr[strlen(s)];
+    char substr[strlen(s)+1];
+    substr[0] = '\0';
     int i = 0;
     int j = 0;
     int k = 0;
@@ -26,7 +29,7 @@ int test(char* s, char* resultStr)
                 left_inner_ptr = left_ptr;
                 right_inner_ptr = right_ptr;
                 
-                if (j == 0)
+                if (j == 1)
                 {
                     substr[0] = s[i+1];
                     substr[1] = '\0';
@@ -51,7 +54,7 @@ int test(char* s, char* resultStr)
                 }
             }
 
-            if (j == 0)
+            if (j == 0 && i < strlen(s)-2)
             {
                 right_ptr++;
             }
@@ -62,14 +65,14 @@ int test(char* s, char* resultStr)
 
             if (count*2-j > strlen(resultStr))
             {
-                for (n = count-j; n > 0; n--)
+                for (n = count; n > 0; n--)
                 {
                     resultStr[k] = substr[n];
                     resultStr[k+1] = '\0';
                     k++;
                 }
                 
-                for (n = 0; n < count; n++)
+                for (n = 0+j; n < count; n++)
                 {
                     resultStr[k] = substr[n];
                     resultStr[k+1] = '\0';
@@ -79,31 +82,31 @@ int test(char* s, char* resultStr)
         }
         left_ptr++;
     }
-}
 
+    for (int p; p < strlen(resultStr); p++)
+    {
+        s[p] = resultStr[p];
+        s[p+1] = '\0';
+    }
 
-char* longestPalindrome(char* s)
-{
-    char* resultStr;
-    char* longestEvenResStr = lookForEvenPalindrome(s, longestEvenResStr);
-    char* longestUnevenResStr = lookForUnevenPalindrome(s, longestUnevenResStr);
-
-    s = strlen(longestEvenResStr) > strlen(longestUnevenResStr) ? longestEvenResStr : longestUnevenResStr;
-
-    return 0;
+    return s;
 }
 
 int main(int argc, char const *argv[])
 {
-    char test_str[] = "lkliaskugdioasdfdadasauasgdiougsalkisdughfixdjyasudhiuhaohtesetholm";
-    char result[strlen(test_str)];
-    result[0] = '\0';
-    char* result_ptr = result;
+    char test_str[] = "aa";
+    // char result[strlen(test_str)];
+    // result[0] = '\0';
+    //char* result_ptr = result;
     //char* test_ptr = &test_ptr[0];
     //longestPalindrome(test_str);
     //lookForUnevenPalindrome(test_str, result_ptr);
-    test(test_str, result_ptr);
 
+    char* result;
+    result = (char*) malloc(strlen(test_str)*sizeof(char)+1);
+
+    result = longestPalindrome(test_str);
+    
     // int i = 0;
     // while (test_str[i] != '\0')
     // {
